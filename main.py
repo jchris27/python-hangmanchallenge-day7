4666652cac4767ab48f8d9d81e2a3617f785c148
Jin <53550425+jchris27@users.noreply.github.com>
2022-03-19 14:06:57 +0000

    added user input and assign it to guess var

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ word_list = ["aardvark", "baboon", "camel"]
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = ""
 
-for word in word_list:
-  chosen_word = random.choice(word_list)
+chosen_word = random.choice(word_list)
 
 print(chosen_word)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+user_input = input("Guess a letter: \n")
+guess = user_input.lower()
+
+print(guess)
+
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for char in chosen_word:
+  if guess == char:
+    print("YES")
